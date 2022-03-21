dc7052c17df21f220326b04edd578f2150035d25
lisiyuan <2998626035@qq.com>
2022-03-21 18:30:34 +0800

    first

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,10 +1,20 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar>
+
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    NavBar
+
+  }
 };
 </script>
 
