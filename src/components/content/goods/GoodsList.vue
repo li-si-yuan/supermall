614a02260f29a937d6b3bcea55068a44db09b82f
lisiyuan <2998626035@qq.com>
2022-03-23 21:11:36 +0800

    second

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="goods">
+    <goods-list-item v-for="item in goods" :key="item" :goods-item="item"></goods-list-item>
+  </div>
+</template>
+
+<script>
+import GoodsListItem from './GoodsListItem.vue';
+
+export default {
+  components: { GoodsListItem },
+  name: 'GoodsList',
+  computed: {
+    GoodsListItem
+  },
+  props: {
+    goods: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style scoped>
+  .goods {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,77 +5,8 @@
     <recommend-view :recommends="recommends"></recommend-view>
     <feature-views></feature-views>
     <tab-control class="tab=control" :titles="['流行','新款','精选']"></tab-control>
-
-    <ul>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-      <li>列表1</li>
-    </ul>
+    <goods-list :goods="goods['pop'].list"></goods-list>
+    
   </div>
 </template>
 
@@ -86,6 +17,7 @@ import FeatureViews from './childComps/FeatureViews'
 
 import NavBar from 'components/common/navbar/NavBar'
 import TabControl from 'components/content/tabControl/TabControl.vue'
+import GoodsList from 'components/content/goods/GoodsList.vue'
 
 import {
   getHomeMultidata,
@@ -98,7 +30,8 @@ export default {
     RecommendView,
     FeatureViews,
     NavBar,
-    TabControl
+    TabControl,
+    GoodsList
 
   },
   data() {
@@ -108,21 +41,34 @@ export default {
       mes: null,
       goods: {
         'pop': {page: 0,list: []},
-        'news': {page: 0,list: []},
+        'new': {page: 0,list: []},
         'sell': {page: 0,list: []},
       }
     }
   },
   created() {
-    getHomeMultidata().then(res => {
-      this.null = res
+    this.getHomeMultidata(),
+    this.getHomeGoods('pop'),
+    this.getHomeGoods('new'),
+    this.getHomeGoods('sell')
+
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+      this.mes = res
       this.banners = res.data.banner.list
       this.recommends = res.data.recommend.list
     })
-    
-    getHomeGoods('pop',1).then(res => {
-        console.log(res);
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1
+      getHomeGoods(type,page).then(res => {
+        this.goods[type].list.push(...res.data.list)
+        // console.log(this.goods.pop);
+        this.goods[type].page += 1
     })
+    }
   }
 };
 </script>
@@ -133,13 +79,14 @@ export default {
   }
   .home-nav {
     position: fixed;
+    z-index: 9;
     left: 0;
     right: 0;
     top: 0;
-    z-index: 9;
   }
   .tab-control {
     position: sticky;
     top: 44px;
+    z-index: 9;
   }
 </style>
